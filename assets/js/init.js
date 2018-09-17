81038b1f79b16936a0b45c6cfb5e0e2ce20f5f7c
Tamas Orosz <bombear1@gmai.com>
2018-09-18 01:00:01 +0200

    services

--- assets/js/init.js
+++ assets/js/init.js
@@ -1,74 +1,61 @@
-$( document ).ready(function() {
+$(document).ready(function () {
   serviceLoader();
 });
 
 
 //setTimeout(function() { mainLoader(); }, 1000);
 
-$(function() {
+$(function () {
 
-  setTimeout(function() {
+  setTimeout(function () {
     $("#subTitle").hide();
-   $("#starterLogoContainer").addClass("topLeft");
-   $("#starterLogo").addClass("rotate");
- }, 500);  
+    $("#starterLogoContainer").addClass("topLeft");
+    $("#starterLogo").addClass("rotate");
+  }, 500);
 
-  setTimeout(function() {
+  setTimeout(function () {
     languageHU();
- }, 500);
+  }, 500);
 
-  setTimeout(function() {
+  setTimeout(function () {
     $("#subTitle").hide();
-   $("#starterLogoBackground").addClass("small");
- }, 500);
+    $("#starterLogoBackground").addClass("small");
+  }, 500);
 
-  setTimeout(function() {
+  setTimeout(function () {
     $("#starterLogoBackground").addClass("fade");
   }, 800);
-  setTimeout(function() {
+  setTimeout(function () {
     $("#starterLogoBackground").hide();
     $("#starterLogoContainer").hide();
 
-  },1100);
+  }, 1100);
 });
 
 
-$(function(){
-     $("footer").load("footer/footer.html");
-   });
+function atmeretezes() {
 
-
-$(function(){
-     $("header").load("menu/mainMenu.html");
-   });
-
-
-
-
-
-function atmeretezes(){
-
-      if (window.pageYOffset>10){
-        $('header').addClass("sticky");
-        $('logo').addClass("stick");
-      }
-      else{
-        $('header').removeClass("sticky");
-        $('logo').removeClass("stick");
-        };
+  if (window.pageYOffset > 10) {
+    $('header').addClass("sticky");
+    $('logo').addClass("stick");
+  }
+  else {
+    $('header').removeClass("sticky");
+    $('logo').removeClass("stick");
   };
+};
 
 
-function visszahozas(){
-  $(document).ready(function(){
-    $(document).mousemove(function(event){
-      var pos=event.clientY;
-      if(pos<92){
+function visszahozas() {
+  $(document).ready(function () {
+    $(document).mousemove(function (event) {
+      var pos = event.clientY;
+      if (pos < 92) {
         $('header').removeClass("sticky");
-        };
+      };
     });
-    });
-  };
+  });
+};
 
 window.addEventListener('scroll', atmeretezes);
 window.addEventListener('mousemove', visszahozas);
@@ -76,51 +63,51 @@ window.addEventListener('mousemove', visszahozas);
 /*MAP*/
 
 function initMap() {
-    var center = {lat: 47.441490, lng: 19.041790}
-    var mapOptions = {
-        center: new google.maps.LatLng(center),
-        zoom: 16,
-        mapTypeId: 'roadmap'
-            }
-    var markerOptions={
-      position: center,
-      map:map
-    }
-
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    var marker = new google.maps.Marker({position: center, map:map})
-
-
-    //EZEK MIÉRT NEM MŰKÖDNEK?????
-    //var marker = new google.maps.Marker(markerOptions)
-    //var map = new google.maps.Map($('#map'), mapOptions);
+  var center = { lat: 47.441490, lng: 19.041790 }
+  var mapOptions = {
+    center: new google.maps.LatLng(center),
+    zoom: 16,
+    mapTypeId: 'roadmap'
   }
+  var markerOptions = {
+    position: center,
+    map: map
+  }
+
+  var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  var marker = new google.maps.Marker({ position: center, map: map })
+
+
+  //EZEK MIÉRT NEM MŰKÖDNEK?????
+  //var marker = new google.maps.Marker(markerOptions)
+  //var map = new google.maps.Map($('#map'), mapOptions);
+}
 
 /*LANGUAGE SELECTOR*/
 
-function languageEN(){
+function languageEN() {
   console.log("EN")
-  $(".HU").addClass( "noDisplay");
-    $(".EN").removeClass( "noDisplay");
+  $(".HU").addClass("noDisplay");
+  $(".EN").removeClass("noDisplay");
 }
 
-function languageHU(){
+function languageHU() {
   console.log("HU")
-  $(".EN").addClass( "noDisplay");
-    $(".HU").removeClass( "noDisplay");
+  $(".EN").addClass("noDisplay");
+  $(".HU").removeClass("noDisplay");
 }
 
-   
+
 
 /*SERVICES*/
 
-function serviceLoader(){
+function serviceLoader() {
   /*beruházók*/
-  $(function(){
-    $(".serviceMenuItem").click(function(e){
-       var displayID = $(this).data('target');
-       $('.serviceContainer').hide();
-       $("#"+displayID+".serviceContainer").css("display", "block");
+  $(function () {
+    $(".serviceMenuItem").click(function (e) {
+      var displayID = $(this).data('target');
+      $('.serviceContainer').hide();
+      $("#" + displayID + ".serviceContainer").css("display", "block");
     });
- });
+  });
 }
